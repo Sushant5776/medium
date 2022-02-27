685a9cfebd0676295b701c43e349d796c5d57452
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-02-28 00:06:35 +0530

    Home and Blog Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Router from 'next/router'
+import nprogress from 'nprogress'
+
+nprogress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => nprogress.start())
+Router.events.on('routeChangeComplete', () => nprogress.done())
+Router.events.on('routeChangeError', () => nprogress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
